b16bae38650aa286862d897d3c30818b77d67e25
Philipp Rentzsch <rentzschp@rki.de>
2015-03-11 16:48:42 +0100

    Refreshed setup/py2exe-compile script

--- plugin_source/setup_py2exe.py
+++ plugin_source/setup_py2exe.py
@@ -5,7 +5,15 @@ import py2exe, sys
 sys.argv.append('py2exe') # so we do not have to add py2exe to the arguments
 
 setup(
-    console=['WeightedSimilarityMatrix.py', 'AbundanceCorrection.py', 'ExcludePeptides.py', 'CountPeptides.py', 'ConcatenateFiles.py', 'IdxmlToFasta.py', 'SixFrameTranslation.py'],
+    console=[
+        'WeightedSimilarityMatrix.py',
+        'AbundanceCorrection.py',
+        'ExcludePeptides.py',
+        'CountPeptides.py',
+        'ConcatenateFiles.py',
+        'IdxmlToFasta.py',
+        'SixFrameTranslation.py',
+        'SubtractIdxml.py'],
     options={
         'py2exe':{
             'includes' : ['scipy.sparse.csgraph._validation']}}) # necessary to prevent error: http://stackoverflow.com/questions/14215303/scipy-with-py2exe
